5643094f8eb0b2cccb2871ed9e4098e2db1033a2
Muhammad Usama <usamabhatti4061@gmail.cgit config --global user.email usamabhatti4061@gmail.c>
2022-03-07 16:35:09 +0500

    Compeleted the CRUD operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -33,10 +33,26 @@ end
 
 def destroy
 
+@article = Article.find(params[:id])
+@article.destroy
+redirect_to articles_path
 
-  
 end
 
+
+def update
+  @article = Article.find(params[:id])
+  if @article.update(params.require(:article).permit(:title, :description))
+    flash[:notice] = "Article was updated succesfully."
+    redirect_to @article
+  else
+    render 'edit'
+  end
+end
+
+def edit
+  @article = Article.find(params[:id])
+end
 
 
 end #class
