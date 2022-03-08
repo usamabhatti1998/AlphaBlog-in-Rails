7c8e373530c0a7d3e2637598b2b7ea03f3178d4d
Muhammad Usama <usamabhatti4061@gmail.cgit config --global user.email usamabhatti4061@gmail.c>
2022-03-08 17:02:15 +0500

    Bootstrap Working

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-//= require_tree .
+import "bootstrap"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-import 'bootstrap'
