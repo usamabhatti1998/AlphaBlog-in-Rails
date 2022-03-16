b442a132c44fbf24d6d52f6bc6570a3e6615ce3e
Usama <muhammadusama@deltalabs.io>
2022-03-16 13:36:27 +0500

    User validations added succesfully

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze   
+
+    validates :username, presence: true, length: { minimum: 3, maximum: 25 }
+    # validates :password, presence: true, length: { minimum: 6, maximum: 255 }
+    validates :email, presence: true, length: { minimum: 10, maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
 end
