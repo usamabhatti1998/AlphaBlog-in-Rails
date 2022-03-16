7bba02a3c0e97af60bf7a604a34df2660a93b109
Usama <muhammadusama@deltalabs.io>
2022-03-16 17:32:36 +0500

    Making changes to Object before save working now

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
+    before_save  { self.email = email.downcase }
     has_many :articles
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze   
 
-    validates :username, presence: true, length: { minimum: 3, maximum: 25 }
+    validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
+
     # validates :password, presence: true, length: { minimum: 6, maximum: 255 }
     validates :email, presence: true, length: { minimum: 10, maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
