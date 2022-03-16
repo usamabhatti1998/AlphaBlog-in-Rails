3ad409b056556e056f2ae7d96ae2d12a59971f73
Usama <muhammadusama@deltalabs.io>
2022-03-16 18:56:27 +0500

    Password being stored in hashed form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
 
     validates :username, presence: true, length: { minimum: 3, maximum: 25 }, uniqueness: { case_sensitive: false }
 
-    # validates :password, presence: true, length: { minimum: 6, maximum: 255 }
+
     validates :email, presence: true, length: { minimum: 10, maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+has_secure_password
 
 end
