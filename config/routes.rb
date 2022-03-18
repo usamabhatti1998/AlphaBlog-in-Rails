9fee789a31aae7af594541bdab2270f55ba519db
Usama <muhammadusama@deltalabs.io>
2022-03-18 19:13:38 +0500

    ..

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :users
+  # resources :users
   root ('home#index')
   get 'about', to: 'home#about'
   get 'signup', to: 'users#new'
-  post 'user', to: 'users#create'
+  post 'users', to: 'users#create'
   resources :users , except: [:new]  #as it has been created above. we wil get all except new
 
 
